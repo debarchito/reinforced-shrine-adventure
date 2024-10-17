b8efa547e11b3bf18495bcab28d48a0c1e4a10fd
HappySR <darrang48@gmail.com>
2024-10-17 20:33:15 +0530

    Added button paths

--- src/init.py
+++ src/init.py
@@ -39,6 +39,33 @@ class UI:
 
     def button_cog_active(self, namehint: str = "") -> pygame.Surface:
         return pygame.image.load("assets/images/ui/button_cog_active.png", namehint)
+    
+    def button_options(self, namehint: str = "") -> pygame.Surface:
+        return pygame.image.load("assets/images/ui/button_options.png", namehint)
+
+    def button_options_hover(self, namehint: str = "") -> pygame.Surface:
+        return pygame.image.load("assets/images/ui/button_options_hover.png", namehint)
+
+    def button_options_active(self, namehint: str = "") -> pygame.Surface:
+        return pygame.image.load("assets/images/ui/button_options_active.png", namehint)
+    
+    def button_credits(self, namehint: str = "") -> pygame.Surface:
+        return pygame.image.load("assets/images/ui/button_credits.png", namehint)
+
+    def button_credits_hover(self, namehint: str = "") -> pygame.Surface:
+        return pygame.image.load("assets/images/ui/button_credits_hover.png", namehint)
+
+    def button_credits_active(self, namehint: str = "") -> pygame.Surface:
+        return pygame.image.load("assets/images/ui/button_credits_active.png", namehint)
+    
+    def button_exit(self, namehint: str = "") -> pygame.Surface:
+        return pygame.image.load("assets/images/ui/button_exit.png", namehint)
+
+    def button_exit_hover(self, namehint: str = "") -> pygame.Surface:
+        return pygame.image.load("assets/images/ui/button_exit_hover.png", namehint)
+
+    def button_exit_active(self, namehint: str = "") -> pygame.Surface:
+        return pygame.image.load("assets/images/ui/button_exit_active.png", namehint)
 
 
 class Images:
